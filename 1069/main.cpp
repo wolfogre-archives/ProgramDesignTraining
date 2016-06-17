4e62c362d0942cab458d380dc31329c5e66baba4
Jason Song <wolfogre@outlook.com>
2016-06-17 11:02:30 +0800

    1069

---
+++ 1069/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <set>
+
+using namespace std;
+
+struct Point
+{
+	Point(double x, double y, double z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	double x;
+	double y;
+	double z;
+};
+
+double distance(const Point &a, const Point &b)
+{
+	return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<Point> input;
+	double x, y, z;
+
+	while(n-- > 0)
+	{
+		cin >> x >> y >> z;
+		input.push_back(Point(x, y, z));
+	}
+
+	cin >> x >> y >> z;
+	Point acm(x, y, z);
+	cin >> x >> y >> z;
+	Point future(x, y, z);
+
+	Point mid = Point((acm.x + future.x) / 2, (acm.y + future.y) / 2, (acm.z + future.z) / 2);
+
+	double min = distance(mid, input[0]);
+	size_t min_index = 0;
+	for(size_t i = 0 + 1; i < input.size(); ++i)
+	{
+		double temp = distance(mid, input[i]);
+		if(temp < min)
+		{
+			min = temp;
+			min_index = i;
+		}
+	}
+
+	cout << fixed << setprecision(3) << input[min_index].x << " " << input[min_index].y << " " << input[min_index].z;
+
+	return 0;
+}
---
+++ 1573/main.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+typedef long long llong;
+
+map<llong, llong> dir;
+
+llong gcb(llong a, llong b)
+{
+	if (b == 0)
+		return a;
+	return gcb(b, a % b);
+}
+
+llong pow(llong a, llong n)//I dont believe the pow() in cmath
+{
+	auto p = dir.find(n);
+	if (p != dir.end())
+		return p->second;
+	llong result = 1;
+	while (n-- > 0)
+		result *= a;
+	dir.insert(pair<llong, llong>(n, result));
+	return result;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while(t-- > 0)
+	{
+		llong n, m;
+		cin >> n >> m;
+		llong sum = 0;
+		dir.clear();
+		for(int k = 1; k <= n; ++k)
+		{
+			cout << k << " " << gcb(n, k) << endl;
+			//sum += pow(m, gcb(n, k)) % 1000000007;
+		}
+			
+		cout << sum << endl;
+	}
+	return 0;
+}
