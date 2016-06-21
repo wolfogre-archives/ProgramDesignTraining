4e1f0863b6778b5b4b470b4c7d7d3bd9b7fd20d3
Jason Song <wolfogre@outlook.com>
2016-06-21 15:54:00 +0800

    1484

---
+++ 1484/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (a < 0)
+		return gcd(-a, b);
+	if (b < 0)
+		return gcd(a, -b);
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+	return abs(a * b / gcd(a, b));
+}
+
+int main()
+{
+	int a, b;
+	int count = 0;
+	while(cin >> a >> b)
+	{
+		if(a == 0 && b == 0)
+		{
+			cout << "Case " << ++count << ":" << endl
+				<< "no GCD" << endl
+				<< "no LCM" << endl << endl;
+			continue;
+		}
+		if (a == 0 || b == 0)
+		{
+			cout << "Case " << ++count << ":" << endl
+				<< "GCD(" << a << "," << b << ") = " << (a == 0 ? b : a) << endl
+				<< "no LCM" << endl << endl;
+			continue;
+		}
+		cout << "Case " << ++count << ":" << endl
+			<< "GCD(" << a << "," << b << ") = " << gcd(a, b) << endl
+			<< "LCM(" << a << "," << b << ") = " << lcm(a, b) << endl << endl;
+	}
+	return 0;
+}
