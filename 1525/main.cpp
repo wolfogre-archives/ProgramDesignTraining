8f53797bcad65bcd28f4ab7bee024751c5315747
Jason Song <wolfogre@outlook.com>
2016-06-22 22:34:49 +0800

    1525,1572

---
+++ 1525/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Z =[N/5] + [N/52] + [N/53] + …
+//http://www.acmerblog.com/factorial-zero-number-5683.html
+
+int count_zero(int n)
+{
+	int result = 0;
+	while ((n /= 5) != 0)
+		result += n;
+	return result;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	while(n-- > 0)
+	{
+		int m;
+		cin >> m;
+		cout << count_zero(m) << endl;
+	}
+	return 0;
+}
---
+++ 1572/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int MOD = 1000000007;
+
+int main()
+{
+	long long f[100000];
+	int n, m;
+	long long g[100000];
+	while (cin >> m >>n)
+	{
+		memset(f, 0, sizeof(f));
+		memset(g, 0, sizeof(g));
+		f[0] = 1;
+		for (int i = 1; i <= n; ++i)
+		{
+			if (i < m)
+			{
+				f[i] = 2 * f[i - 1] % MOD;
+				g[i] = 0;
+				continue;
+			}
+			if (i == m)
+			{
+				f[i] = 2 * f[i - 1] - 1 % MOD;
+				g[i] = 1;
+				continue;
+			}
+			f[i] = (2 * f[i - 1] - f[i - m - 1] + MOD) % MOD;
+			g[i] = (2 * g[i - 1] + f[i - m - 1] + MOD) % MOD;
+		}
+		cout << g[n] << endl;
+	}
+
+	return 0;
+}
