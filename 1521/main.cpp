c10cac73a3a5f38e8b5e98dd711078d84f15ebab
Jason Song <wolfogre@outlook.com>
2016-06-23 00:15:18 +0800

    1521,1539,1664

---
+++ 1521/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//C(N,3) - Sum{ D[i]*(N-1-D[i])/2 | i=1..N }
+//https://www.byvoid.com/blog/poi-1997-tro
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t-- > 0)
+	{
+		int n, m;
+		cin >> n >> m;
+		vector <pair<int, int>> input;
+		for(int i = 0; i < m; ++i)
+		{
+			int temp1, temp2;
+			cin >> temp1 >> temp2;
+			input.push_back(pair<int, int>(temp1, temp2));
+		}
+
+		vector<int> connect_red_num(n);
+		for(auto p : input)
+		{
+			++connect_red_num[p.first - 1];
+			++connect_red_num[p.second - 1];
+		}
+
+		double result = n * (n - 1) * (n - 2) / 6;
+		for(auto it : connect_red_num)
+			result -= it * (n - 1 - it) / 2.0;
+		cout << result << endl;
+	}
+	
+	return 0;
+}
---
+++ 1664/main.cpp
@@ -0,0 +1,32 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	char str[20];
+	while (scanf("%d", &n) != EOF)
+	{
+		int length = 0;
+		while(n != 0)
+		{
+			str[length++] = (n % 3) + '0';
+			n /= 3;
+		}
+		if (length == 0)
+		{
+			putchar('0');
+			putchar('\n');
+		}
+		else
+		{
+			for(int i = length - 1; i >= 0; --i)
+				putchar(str[i]);
+			putchar('\n');
+		}
+	}
+	return 0;
+}
