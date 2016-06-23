e82e6df2ec7daff4624ef146193eb56fa04cfa54
Jason Song <wolfogre@outlook.com>
2016-06-23 16:19:40 +0800

    1629,1639

---
+++ 1629/main.cpp
@@ -0,0 +1,95 @@
+﻿//http://blog.csdn.net/sunnylinner/article/details/51730894
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int mod = 1000000007;
+typedef long long llong;
+const int n = 5;
+
+
+struct modatrix
+{
+	llong data[6][6];
+}origin, res, tmp, A, ans;
+
+modatrix mul(modatrix x, modatrix y)
+{
+	int i, j, k;
+	memset(tmp.data, 0, sizeof(tmp.data));
+	for (i = 1; i <= n; i++)
+		for (j = 1; j <= n; j++)
+			for (k = 1; k <= n; k++)
+			{
+				tmp.data[i][j] += (x.data[i][k] * y.data[k][j]) % mod;
+				tmp.data[i][j] %= mod;
+			}
+	return tmp;
+}
+
+
+
+void quickpow(llong k)
+{
+	int i;
+	memset(res.data, 0, sizeof(res.data));
+	for (i = 1; i <= n; i++)
+		res.data[i][i] = 1;
+	while (k)
+	{
+		if (k & 1)
+			res = mul(res, A);
+		A = mul(A, A);
+		k >>= 1;
+	}
+}
+
+void check()
+{
+	return;
+}
+
+int main()
+{
+	llong n;
+	
+	while (cin >> n) {
+		check();
+		llong a0, ax, ay, b0, bx, by;
+		check();
+		cin >> a0 >> ax >> ay;
+		check();
+		cin >> b0 >> bx >> by; check();
+
+		llong f1, a1, b1, s0; check();
+		a1 = (a0*ax + ay) % mod; check();
+		b1 = (b0*bx + by) % mod; check();
+		f1 = (a1*b1) % mod;
+		s0 = 0;
+		memset(origin.data, 0, sizeof(origin.data)); check();
+		origin.data[1][1] = f1; check();
+		origin.data[1][2] = a1; check();
+		origin.data[1][3] = b1; check();
+		origin.data[1][4] = 1; check();
+		origin.data[1][5] = s0; check();
+		memset(A.data, 0, sizeof(A.data)); check();
+		A.data[1][1] = (ax*bx) % mod; check();
+		A.data[1][5] = 1; check();
+		A.data[2][1] = (ax*by) % mod; check();
+		A.data[2][2] = ax%mod; check(); check();
+		A.data[3][1] = (ay*bx) % mod; check();
+		A.data[3][3] = bx%mod; check();
+		A.data[4][1] = (ay*by) % mod; check();
+		A.data[4][2] = ay%mod; check();
+		A.data[4][3] = by%mod; check();
+		A.data[4][4] = 1; check();
+		A.data[5][5] = 1; check();
+
+		quickpow(n); check();
+		ans = mul(origin, res); check();
+		cout << ans.data[1][5] << endl;
+	}
+	return 0;
+}
---
+++ 1639/main.cpp
@@ -0,0 +1,132 @@
+﻿//http://www.acmerblog.com/hdu-4445-crazy-tank-7458.html
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+typedef double db;
+
+const db g = 9.8;
+const db pi = acos(-1.0);
+const db eps = 1e-8;
+
+db distance(db ang, db v, db h)
+{
+	db s = sin(ang);
+	db c = cos(ang);
+	return v * s * (v * c + sqrt(v * v * c * c + 2.0 * g * h)) / g;
+}
+
+db tris(db v ,db h)
+{
+	db l = 0, r = pi / 2.0;
+	int t = 30;
+	while (t-- > 0)
+	{
+		db mid = (l + r) / 2.0;
+		db midd = (l + mid) / 2.0;
+		if (distance(midd, v, h) > distance(mid, v, h))
+			r = mid;
+		else
+			l = midd;
+	}
+	return l;
+}
+
+db bis_left(db l, db r, db d, db v, db h)
+{
+	int t = 30;
+	while (t-- > 0)
+	{
+		db mid = (l + r) / 2.0;
+		if (distance(mid, v, h) <= d)
+			l = mid;
+		else
+			r = mid;
+	}
+	return l;
+}
+
+db bis_right(db l, db r, db d, db v, db h)
+{
+	return bis_left(r, l, d, v, h);
+}
+
+
+int main()
+{
+	int n;
+	while (cin >> n && n != 0)
+	{
+		db h;
+		vector<db> v;
+		db pos[4];
+		cin >> h >> pos[0] >> pos[1] >> pos[2] >> pos[3];
+		for (int i = 0; i < n; i++)
+		{
+			db t;
+			cin >> t;
+			v.push_back(t);
+		}
+		int res = 0;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < 4; j++)
+
+			{
+				db farest = tris(v[i], h);
+				if (distance(farest, v[i], h) < pos[j])
+					continue;
+				db w = pos[j];
+				if (j == 0)
+					w += eps;
+				if (j == 1)
+					w -= eps;
+				if (j == 2)
+					w -= eps;
+				if (j == 3)
+					w += eps;
+				db ang = bis_left(0.0, farest, w, v[i], h);
+				bool flag = true;
+				int c = 0;
+				for (int k = 0; k < n; k++)
+				{
+					db here = distance(ang, v[k], h);
+					if (here >= pos[2] && here <= pos[3])
+					{
+						flag = false;
+						break;
+					}
+					if (here >= pos[0] && here <= pos[1])
+						++c;
+				}
+				if (flag) res = max(res, c);
+				if (res == n) break;
+
+				ang = bis_right(farest, pi, w, v[i], h);
+				flag = true;
+
+				c = 0;
+				for (int k = 0; k < n; k++)
+				{
+					db here = distance(ang, v[k], h);
+					if (here >= pos[2] && here <= pos[3])
+					{
+						flag = false;
+						break;
+					}
+					if (here >= pos[0] && here <= pos[1])
+						++c;
+				}
+				if (flag) res = max(res, c);
+				if (res == n)
+					break;
+			}
+		}
+		cout << res << endl;
+	}
+	return 0;
+}
