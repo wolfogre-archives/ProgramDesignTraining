65c8594b47906aea1800d8b0454ce51f8f6a1440
Jason Song <wolfogre@outlook.com>
2016-06-25 13:19:47 +0800

    1321,1857,1875

--- 1857/main.cpp
+++ 1857/main.cpp
@@ -1,11 +1,242 @@
-﻿#error 1857 not finished
-
-#include <iostream>
-
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cmath>
+#include <functional>
 using namespace std;
+
+bool useless(vector<int> &input)
+{
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	for (size_t i = input.size(); i > 0; --i)
+	{
+		for (size_t j = 0; j < i - 1; ++j)
+		{
+			if (input[j + 1] - 1 > input[j])
+			{
+				int temp = input[j];
+				input[j] = input[j + 1];
+				input[j + 1] = temp;
+				--input[j];
+				++input[j + 1];
+			}
+		}
+	}
+
+	for (size_t i = 0; i < input.size() - 1; ++i)
+	{
+		if (input[i] < input[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+
+const int maxN = 105;
+const int MOD = 1000007;
+int n, s[maxN];
+
+#define REP(X,N) for(int X=0;X<N;X++)
+#define REP2(X,L,R) for(int X=L;X<=R;X++)
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> PII;
+typedef priority_queue<int, vector<int>, greater<int> > PQI;
+typedef vector<PII> VII;
+typedef vector<int> VI;
+#define X first
+#define Y second
+
+#define MOD 10007
+
+typedef struct
+{
+	int data[4][4];
+} M;
+
+M I = { 1,0,0,0,         //单位矩阵 
+0,1,0,0,
+0,0,1,0,
+0,0,0,1 };
+
+M juzhengcheng(M a, M b);
+int kuaisumi(M a, int n);
 
 int main()
 {
-
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		int n, x, y;
+		cin >> n >> x >> y;
+		x = x%MOD; y = y%MOD;
+		M a = { 1,(x*x) % MOD,(y*y) % MOD,(2 * x*y) % MOD,
+			0,(x*x) % MOD,(y*y) % MOD,(2 * x*y) % MOD,
+			0,1,0,0,
+			0,x,0,y };
+		cout << kuaisumi(a, n - 1) << endl;
+	}
 	return 0;
+}
+
+M juzhengcheng(M a, M b)
+{
+	M xres = I;
+	REP(i, 4)
+	{
+		REP(j, 4)
+		{
+			int tmp = 0;
+			REP(k, 4)
+				tmp = (tmp + (a.data[i][k] * b.data[k][j]) % MOD) % MOD;
+			xres.data[i][j] = tmp;
+		}
+	}
+	return xres;
+}
+
+
+int kuaisumi(M a, int n)    //快速幂 
+{
+	M xres = I;
+	while (n>0)
+	{
+		if (n & 1)
+			xres = juzhengcheng(xres, a);
+		a = juzhengcheng(a, a);
+		n >>= 1;
+	}
+	int xans = 0;
+	REP(j, 4)
+		xans = (xans + xres.data[0][j]) % MOD;   //将最终解结果的第一行相加 
+	return xans;
 }
--- 1875/main.cpp
+++ 1875/main.cpp
@@ -1,6 +1,11 @@
-﻿#include<iostream>
-#include <cstring>
-#include <vector>
+﻿#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <string>
+#define LL long long
 
 using namespace std;
 
@@ -156,78 +161,60 @@ bool useless(vector<int> &input)
 	return true;
 }
 
+const long long maxN = 105;
+const long long MOD = 1000007;
+long long n, s[maxN];
 
-const int maxN = 105;
-const int MOD = 1000007;
-int n, s[maxN];
-
-const long a[66] = { 0,2,3,5,7,11,13,17,19,23,29,31,37,41,43,
-47,53,59,61,67,71,73,79,83,89,97,101,103,
-107,109,113,127,131,137,139,149,151,157,
-163,167,173,179,181,191,193,197,199,211,
-223,227,229,233,239,241,251,257,263,269,
-271,277,281,283,293,307,311,313 };
-
-void chengcheng(long* an, long n1)
+void input()
 {
-	long i;
-	for (i = 1; i <= an[0]; i++) an[i] = an[i] * n1;
-	for (i = 1; i<an[0]; i++) { an[i + 1] += an[i] / 10; an[i] %= 10; }
-	while (an[an[0]]>9) {
-		an[an[0] + 1] = an[an[0]] / 10;
-		an[an[0]] %= 10;
-		an[0]++;
-	}
-	if (an[0]>101) an[0] = 101;
+	for (long long i = 0; i < n; ++i)
+		cin >> s[i];
 }
 
-int main() {
-	long j, n1, m1, all, h[200000];
-	long i, nn1, mm1, *answer1 = new long[100];
+long long xorGauss(long long n)
+{
+	long long row = 0;
+	for (long long i = 64; i >= 0; i--)
+	{
+		long long j;
+		for (j = row; j < n; j++)
+			if (s[j] & (1ll << i))
+				break;
+		if (j != n)
+		{
+			swap(s[row], s[j]);
+			for (j = 0; j < n; j++)
+			{
+				if (j == row) continue;
+				if (s[j] & (1ll << i))
+					s[j] ^= s[row];
+			}
+			row++;
+		}
+	}
+	return row;
+}
 
-	cin >> n1 >> m1;
-	all = n1 + m1;
-	memset(h, 0, sizeof(h));
-	for (i = n1 + 1; i <= all; i++)
-	{
-		nn1 = i;
-		for (j = 1; j <= 65; j++)
-		{
-			while (nn1%a[j] == 0)
-			{
-				h[a[j]]++; nn1 /= a[j];
-				if (nn1 == 1) break;
-			}
-			if (nn1 == 1) break;
-		}
-		if (nn1 != 1) h[nn1]++;
+void work()
+{
+	long long row, ans, k;
+	row = xorGauss(n);
+	ans = n - row;
+	if (ans >0) {
+		printf("YES\n");
 	}
-	for (i = 1; i <= m1; i++)
-	{
-		mm1 = i;
-		for (j = 1; j <= 65; j++)
-		{
-			while (mm1%a[j] == 0)
-			{
-				h[a[j]]--; mm1 /= a[j];
-				if (mm1 == 1) break;
-			}
-			if (mm1 == 1) break;
-		}
-		if (mm1 != 1) h[mm1]--;
-	}
-	answer1[0] = 1; answer1[1] = 1;
-	for (i = 1; i <= 100000; i++)
-		while (h[i]>0)
-		{
-			chengcheng(answer1, i);
-			h[i]--;
-		}
-	for (i = 100; i >= 1; i--) {
-		cout << answer1[i];
-		if (i % 10 == 1)
-			cout << endl;
+	else {
+		printf("NO\n");
 	}
 }
 
-
+int main()
+{
+	//freopen("test.in", "r", stdin);
+	while (cin >> n)
+	{
+		input();
+		work();
+	}
+	return 0;
+}
