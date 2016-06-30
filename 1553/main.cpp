333879989e6709797664d8e5f03cb6c7a48e8c10
Jason Song <wolfogre@outlook.com>
2016-06-30 13:24:33 +0800

    1553

---
+++ 1553/main.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int mat[100][100];
+int row, col;
+int record[100][100];
+
+int max_length_from(int r , int c)
+{
+	if (record[r][c] != -1)
+		return record[r][c];
+
+	int max = 1;
+	if (r + 1 < row && mat[r][c] > mat[r + 1][c])
+	{
+		int temp = max_length_from(r + 1, c) + 1;
+		if (temp > max)
+			max = temp;
+	}
+	if (c + 1 < col && mat[r][c] > mat[r][c + 1])
+	{
+		int temp = max_length_from(r, c + 1) + 1;
+		if (temp > max)
+			max = temp;
+	}
+	if (r - 1 >= 0 && mat[r][c] > mat[r - 1][c])
+	{
+		int temp = max_length_from(r - 1, c) + 1;
+		if (temp > max)
+			max = temp;
+	}
+	if (c - 1 >= 0 && mat[r][c] > mat[r][c - 1])
+	{
+		int temp = max_length_from(r, c - 1) + 1;
+		if (temp > max)
+			max = temp;
+	}
+	record[r][c] = max;
+	return max;
+}
+
+int max_length()
+{
+	int max = -1;
+	for (int i = 0; i < row; ++i)
+		for (int j = 0; j < col; ++j)
+		{
+			int temp = max_length_from(i, j);
+			if (max < temp)
+				max = temp;
+		}
+	return max;
+}
+
+int main()
+{
+	cin >> row >> col;
+	for (int i = 0; i < row; ++i)
+		for (int j = 0; j < col; ++j)
+		{
+			record[i][j] = -1;
+			cin >> mat[i][j];
+		}
+	cout << max_length() << endl;
+	return 0;
+}
---
+++ 1611/main.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int record[21][21];
+vector<int> input;
+
+#error 1611 not work
+
+int get_min_time_to_mul(int start, int end)
+{
+	if (start == end)
+	{
+		cout << start << " " << end << " " << input[start - 1] * input[start] * input[start + 1] << endl;;
+		return input[start - 1] * input[start] * input[start + 1];
+	}
+		
+	if (record[start][end] != -1)
+		return record[start][end];
+	int min = INT32_MAX;
+	for(int mid = start; mid < end; ++mid)
+	{
+		int temp = get_min_time_to_mul(start, mid) + get_min_time_to_mul(mid + 1, end) + input[start - 1] * input[mid + 1] * input[end + 1];
+		if (temp < min)
+			min = temp;
+	}
+	record[start][end] = min;
+	cout << start << " " << end << " " << min << endl;;
+	return min;
+}
+
+
+int get_min_time()
+{
+	return get_min_time_to_mul(1, input.size() - 2);
+}
+
+
+int main()
+{
+	int n;
+	while(cin >> n)
+	{
+		++n;
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				record[i][j] = -1;
+		input.clear();
+		while(n-- > 0)
+		{
+			int temp;
+			cin >> temp;
+			input.push_back(temp);
+		}
+		cout << get_min_time() << endl;
+		
+
+	}
+	return 0;
+}
