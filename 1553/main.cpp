6c952bd1021690686d9eac6d9c23be33f9a6b97f
Jason Song <wolfogre@outlook.com>
2016-07-06 16:28:54 +0800

    报告要用的注释

--- 1553/main.cpp
+++ 1553/main.cpp
@@ -6,62 +6,93 @@ int mat[100][100];
 int row, col;
 int record[100][100];
 
+
+//计算从点(r,c)滑到底的最长距离
 int max_length_from(int r , int c)
 {
+	//若备忘录中存在计算结果,则返回该结果
 	if (record[r][c] != -1)
 		return record[r][c];
 
+	//记录最优值max初始值为1,即若不存在可以滑向的相邻点,则点的最优值为1
 	int max = 1;
+
+	//若下方的点可以滑向
 	if (r + 1 < row && mat[r][c] > mat[r + 1][c])
 	{
+		//计算下方的点滑到底的最长距离加1,更新max
 		int temp = max_length_from(r + 1, c) + 1;
 		if (temp > max)
 			max = temp;
 	}
+	//若右方的点可以滑向
 	if (c + 1 < col && mat[r][c] > mat[r][c + 1])
 	{
+		//计算右方的点滑到底的最长距离加1,更新max
 		int temp = max_length_from(r, c + 1) + 1;
 		if (temp > max)
 			max = temp;
 	}
+	//若上方的点可以滑向
 	if (r - 1 >= 0 && mat[r][c] > mat[r - 1][c])
 	{
+		//计算上方的点滑到底的最长距离加1,更新max
 		int temp = max_length_from(r - 1, c) + 1;
 		if (temp > max)
 			max = temp;
 	}
+	//若左方的点可以滑向
 	if (c - 1 >= 0 && mat[r][c] > mat[r][c - 1])
 	{
+		//计算左方的点滑到底的最长距离加1,更新max
 		int temp = max_length_from(r, c - 1) + 1;
 		if (temp > max)
 			max = temp;
 	}
+
+	//记录最优值到备忘录
 	record[r][c] = max;
+	//返回最优值
 	return max;
 }
 
+//计算所有点滑到底的最长距离的最大值
 int max_length()
 {
+	//记录最大值初始值为-1
 	int max = -1;
+
+	//遍历所有点
 	for (int i = 0; i < row; ++i)
 		for (int j = 0; j < col; ++j)
 		{
+			//计算该点滑到底的最长距离
 			int temp = max_length_from(i, j);
+			//更新最大值
 			if (max < temp)
 				max = temp;
 		}
+
+	//返回最大值
 	return max;
 }
 
+//主函数
 int main()
 {
+	//读入行列
 	cin >> row >> col;
+
 	for (int i = 0; i < row; ++i)
 		for (int j = 0; j < col; ++j)
 		{
+			//初始化备忘录
 			record[i][j] = -1;
+			//读入点的值
 			cin >> mat[i][j];
 		}
+
+	//输出所有点滑到底的最长距离的最大值
 	cout << max_length() << endl;
 	return 0;
 }
--- 1997/main.cpp
+++ 1997/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+
 struct Food
 {
 	int w;
@@ -11,42 +11,44 @@ struct Food
 };
 
 vector<Food> foods;
-vector<bool> buy;
 
 int n, m, s;
-
 int min_price;
-
 int price;
-
 int weight;
-
 int buy_count;
 
+//搜索解空间树的最优解
 void get_min_price(size_t index)
 {
+	//若已经到达解空间树的叶节点以下
 	if(index >= foods.size())
 	{
+		//记录得到解的最低价格并返回
 		min_price = price;
 		return;
 	}
 
-	buy[index] = true;
-	if (price + foods[index].c < min_price && weight + foods[index].w <= s && foods[index].t > buy_count)
+	//若满足"买"的约束条件和限定条件:
+	//1.该食物的价格加上已买的食物的总价格不超过已找到的解的最低价格；
+	//2.该食物的重量加上已买的食物的总重量不超过能背负的最大重量；
+	//3.该食物的保质期大于已经买的食物的份数；
+	if (price + foods[index].c < min_price && weight + foods[index].w <= s
+		&& foods[index].t > buy_count)
 	{
-
-		price += foods[index].c;
-		weight += foods[index].w;
-		buy_count += 1;
-		get_min_price(index + 1);
-		price -= foods[index].c;
-		weight -= foods[index].w;
-		buy_count -= 1;
+		price += foods[index].c;//已购买的食物总价格自加当前食物价格
+		weight += foods[index].w;//已购买的食物总重量自加当前食物重量
+		buy_count += 1;//已购买的食物份数增1
+		get_min_price(index + 1);//求解左子树的解空间
+		buy_count -= 1;//已购买的食物份数减1
+		weight -= foods[index].w;//已购买的食物总重量自减当前食物重量
+		price -= foods[index].c;//已购买的食物总价格自减当前食物价格
 	}
-		
-	buy[index] = false;
+
+	//若满足"不买"的约束条件:
+	//剩余的可买食物份数加上已买的食物份数大于需要购买的总份数
 	if(foods.size() - 1 - index + buy_count >= n)
-		get_min_price(index + 1);
+		get_min_price(index + 1);//求解右子树的解空间
 }
 
 int main()
@@ -55,33 +57,44 @@ int main()
 	cin >> T;
 	while(T-- > 0)
 	{
+		//清除上组测试数据读入的食物信息
 		foods.clear();
-		buy.clear();
+		//读入需要购买的食物份数,商店拥有的食物份数,食物总重量的最大值
 		cin >> n >> m >> s;
 		while(m-- > 0)
 		{
-			buy.push_back(false);
 			Food food;
+			//读入一份新食物的信息
 			cin >> food.w >> food.t >> food.c;
-			bool done = false;
+			//记录是否已经插入到食物信息数组
+			bool inserted = false;
+			//遍历食物信息数组
 			for(auto it = foods.begin(); it != foods.end(); ++it)
+				//若食物信息数组中某一份食物的保质期大于新食物的保质期
 				if(it->t > food.t)
 				{
+					//将新食物插入到该食物的前面
 					foods.insert(it, food);
-					done = true;
+					//记录已经插入
+					inserted = true;
 					break;
 				}
-			if (!done)
+			//若没有插入
+			if (!inserted)
+				//追加到食物信息数组末尾
 				foods.push_back(food);
 		}
+		//初始化最低价格为无穷大
 		min_price = INT32_MAX;
-		price = 0;
-		weight = 0;
-		buy_count = 0;
+		//初始化已购买食物总价格,总重量,数量为0
+		price = weight = buy_count = 0;
+		//搜索解空间树
 		get_min_price(0);
+
+		//如果最低价格仍为无穷大,即无解
 		if (min_price == INT32_MAX)
 			cout << -1 << endl;
-		else
+		else//若有解
 			cout << min_price << endl;
 	}
 	return 0;
